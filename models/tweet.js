9c7ab9eb37cd7170906218c2b0d15d49d846c9d7
duchiniandrew <32436381+duchiniandrew@users.noreply.github.com>
2017-10-02 15:53:36 -0300

    adicionado comentarios

--- models/tweet.js
+++ models/tweet.js
@@ -1,13 +1,13 @@
 module.exports = () => {
-	const mongoose     = require('mongoose');
-	const Schema       = mongoose.Schema;
+	const mongoose     = require('mongoose'); //Adiciona a biblioteca para realizar operacoes no bando de dados mongoDB
+	const Schema       = mongoose.Schema; //Adiciona o esquema de uso da biblioteca mongoose
 
-	var tweet  = new Schema({
-        texto	   : String,
-        hashTag    : String,
-        internauta : String,
-		cadastro: {type: Date, default: Date.now}
+	var tweet  = new Schema({//Cria uma variavel Tweet 
+        texto	   : String, //Esta variavel tweet ira possui atributos como texto
+        hashTag    : String, //Um atributo hashtag
+        internauta : String,// Um atributo para saber quem foi o usuario que escreveu o tweet
+		cadastro: {type: Date, default: Date.now}// Um cadastro para pegar a data para se ter referencia de quando foi coletado esse tweet
 	});
 
-	return mongoose.model('tweet', tweet);
+	return mongoose.model('tweet', tweet); //Retorna a variavel tweet que foi criado, pois esse script sera acessado para se requisitar uma variavel tweet para ser preenchida e armazenada na base de dados
 }
