d967dcb399ca34cd57d0caae5d0db03ab2acf8a6
duchiniandrew <32436381+duchiniandrew@users.noreply.github.com>
2017-10-02 15:54:27 -0300

    Adicionados comentarios

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express        = require('express');
+const express        = require('express'); 
 const favicon        = require('serve-favicon');
 const load           = require('express-load');
 const logger         = require('morgan');
 const cookieParser   = require('cookie-parser');
 const bodyParser     = require('body-parser');
-const port           = 3000;
-var   posicao        = 0;
+const port           = 3000; //Seta a porta que o servidor ira conectar
+var   posicao        = 0; //Variavel usada na leitura da lista de congressistas, comecando na primeira posicao da lista
 
 var app = express();
 
 load('models').then("dao").then('middleware').then("service").into(app);
 
-var mongodb = app.middleware.mongodb;
-var service = app.service.service;
+var mongodb = app.middleware.mongodb; //Variavel que ira receber a conexao do banco de dados em mongoDB que foi criada no script middleware.mongoDB
+var service = app.service.service; //Variavel que ira receber o servico que foi criado no script service.service
 
 // view engine setup
 app.set('views', __dirname + '/views');
@@ -23,15 +23,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
 
-mongodb.connect();
-service.saveParlamentar();
+mongodb.connect();//Estabelece a conexao com o banco de dados MongoDB
+service.saveParlamentar(); //Salva o congressitas 
 
 if(posicao <= 593){
-    setInterval( () => { 
-        service.getParlamentar(posicao++);
-    },5000);
+    setInterval( () => { //Adiciona um intervalo entre cada busca, pois a API possui um limite de requisicoes que pdoem ser feitas nela
+        service.getParlamentar(posicao++); //Pula para a proxima posicao da lista de congressistas a serem pesquisados os tweets
+    },5000); //seta o intervalo de 5000 milisegundos (5 segundo)
 }
 
-app.listen(port, () => {
-    console.log("Servidor rodando na porta "+port);
+app.listen(port, () => { //Faz a aplicacao escutar a porta e ver se ela esta disponivel
+    console.log("Servidor rodando na porta "+port); //Exibe ao desenvolvedor a porta ao qual o servidor esta conectado
 });
