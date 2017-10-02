7e712d94031123d0f482d16833db0000f4d57566
duchiniandrew <32436381+duchiniandrew@users.noreply.github.com>
2017-10-02 15:54:05 -0300

    adicionado comentarios

--- models/congressistas.js
+++ models/congressistas.js
@@ -1,11 +1,11 @@
-module.exports = () => {
-	const mongoose     = require('mongoose');
-	const Schema       = mongoose.Schema;
+module.exports = () => {//Funcao que ira criar variaveis do tipo congressista para serem armazenados no banco de dados
+	const mongoose     = require('mongoose'); //Adiciona a biblioteca necessaria para realizar operacoes no banco de dados MongoDB
+	const Schema       = mongoose.Schema; //Adiciona o esquema da biblioteca mongoose para realizar as operacoes
 
-	var congressistas  = new Schema({
-		nome	: String,
-		cadastro: {type: Date, default: Date.now}
+	var congressistas  = new Schema({ //Cria uma variavel congressistas que possuira alguns atributos
+		nome	: String,//Atributo nome da variavel congressista, usado para identificar quem e o congressista
+		cadastro: {type: Date, default: Date.now} //Variavel de cadastro usada para identificar a data de quando foi coletado esse congressista
 	});
 
-	return mongoose.model('congressistas', congressistas);
+	return mongoose.model('congressistas', congressistas); //Cria um modelo no banco de dados do tipo congressista
 }
