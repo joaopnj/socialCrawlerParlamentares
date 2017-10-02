10dfc9cd2a6096f983b393a30e971fc1a5a5fdc0
duchiniandrew <32436381+duchiniandrew@users.noreply.github.com>
2017-10-02 15:52:18 -0300

    Comentarios adicionados

--- middleware/mongodb.js
+++ middleware/mongodb.js
@@ -1,14 +1,14 @@
 module.exports = (app) => { 
     
-    const mongoose = require('mongoose');
+    const mongoose = require('mongoose'); //Adiciona a biblioteca mongoose para se realizar operacoes no banco de dados mongoDB
     
-    var MongoDbMiddleware = {
-        connect : () => {
-            mongoose.connect('mongodb://localhost/trabalhoBD', (err) => {
-                if(err){ console.log('Erro ao conectar no mongodb '+err); }
+    var MongoDbMiddleware = { //Cria uma variavel que ira possuir os dados da conexao com o bando de dados 
+        connect : () => {//Funcao que ira realizar a conexao com o banco de dados
+            mongoose.connect('mongodb://localhost/trabalhoBD', (err) => { //Pega o endereco do servidor de dados e cria a conexao com ele
+                if(err){ console.log('Erro ao conectar no mongodb '+err); } //Caso ocorra erro na conexao, exibe ao desenvolvedor o erro ocorrido
             });
         }
     }
     
-    return MongoDbMiddleware;
+    return MongoDbMiddleware; //Retorna a variavel que possuira os dados da conexao com o banco de dados
 }
